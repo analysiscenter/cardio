0d8fdbc6126ea864ae9e3d25faa09909fe110365
Roman Kh <rhudor@gmail.com>
2017-04-05 14:10:23 +0300

    Signal is not padded, Added create_annotation_df

--- batch/ecg_batch.py
+++ batch/ecg_batch.py
@@ -1,8 +1,7 @@
-""" contain Batch class for storing ECGs """
+""" contain Batch class for processing ECGs """
 
-import sys
 import os
-from copy import deepcopy
+import sys
 import numpy as np
 import pandas as pd
 import wfdb
@@ -13,43 +12,37 @@ from dataset import Batch, action
 
 class EcgBatch(Batch):
     """
-    TODO
+    Batch of ECG data
     """
     def __init__(self, index):
         super().__init__(index)
 
         self._data = None
-        self._annotation = pd.DataFrame(columns=["ecg", "index", "value"])
+        self._annotation = self.create_annotation_df()
         self._meta = dict()
-        self._ecg_index_path = dict()
-        self._ecg_index_number = dict()
         self.history = []
+
+    @staticmethod
+    def create_annotation_df(data=None):
+        """ Create a pandas dataframe with ECG annotations """
+        return pd.DataFrame(data=data, columns=["ecg", "index", "value"])
 
     @action
     def load(self, src, fmt="wfdb"):
         """
         Loads data from different sources
+        src is not used yet, so files locations are defined by the index
         """
-        all_ecg_paths = src
-        self._ecg_index_path = {ecg: all_ecg_paths[ecg] for ecg in self.index}
-        self._ecg_index_number = {
-            self.index[i]: i
-            for i in range(len(self.index))
-        }
-
         if fmt == "wfdb":
-            list_of_arrs, list_of_annotations = self._load_wfdb()
+            list_of_arrs, list_of_annotations, meta = self._load_wfdb()
         elif fmt == "npz":
-            list_of_arrs, list_of_annotations = self._load_npz()
+            list_of_arrs, list_of_annotations, meta = self._load_npz()
         else:
-            raise TypeError("Incorrect type of batch source")
+            raise TypeError("Incorrect type of source")
 
-        max_length = np.max([a.shape[1] for a in list_of_arrs])
-        self._data = np.asarray([
-            np.pad(sig, ((0, 0), (0, max_length - sig.shape[1])), "constant")
-            for sig in list_of_arrs
-        ])
+        self._data = np.array(list_of_arrs)
         self._annotation = pd.concat(list_of_annotations)
+        self._meta = meta
 
         # add info in self.history
         info = dict()
@@ -62,102 +55,56 @@ class EcgBatch(Batch):
     def _load_wfdb(self):
         list_of_arrs = []
         list_of_annotations = []
-        for ecg in self.index:
-            path = self._ecg_index_path[ecg]
-            sig, fields = wfdb.rdsamp(path)
-            sig = sig.reshape((sig.shape[1], -1))
+        meta = {}
+        for pos, ecg in np.ndenumerate(self.indices):
+            path = self.index.get_fullpath(ecg)
+            signal, fields = wfdb.rdsamp(path)
+            signal = signal.T
             try:
                 annot = wfdb.rdann(path, "atr")
             except FileNotFoundError:
-                annot = pd.DataFrame(columns=["ecg", "index", "value"])     # pylint: disable=redefined-variable-type
-            fields.update({"init_length": sig.shape[1]})
-            list_of_arrs.append(sig)
+                annot = self.create_annotation_df()     # pylint: disable=redefined-variable-type
+            list_of_arrs.append(signal)
             list_of_annotations.append(annot)
-            self._meta.update({self._ecg_index_number[ecg]: fields})
+            fields.update({"__pos": pos[0]})
+            meta.update({ecg: fields})
 
-        return list_of_arrs, list_of_annotations
+        return list_of_arrs, list_of_annotations, meta
 
     def _load_npz(self):
         list_of_arrs = []
         list_of_annotations = []
-        for ecg in self.index:
-            path = self._ecg_index_path[ecg]
-            data = np.load(path+".npz")
-            list_of_arrs.append(data["arr_0"])
-            list_of_annotations.append(
-                pd.DataFrame(data["arr_1"], columns=["ecg", "index", "value"])
-            )
-            fields = deepcopy(data["arr_2"].item())
-            fields.update({"init_length": data["arr_0"].shape[1]})
-            self._meta.update({self._ecg_index_number[ecg]: fields})
-        return list_of_arrs, list_of_annotations
-
-    def _load_arrays(self):
-        raise NotImplementedError()
+        meta = {}
+        for pos, ecg in np.ndenumerate(self.indices):
+            path = self.index.get_fullpath(ecg)
+            data = np.load(path)
+            list_of_arrs.append(data["signal"])
+            list_of_annotations.append(self.create_annotation_df(data["annotation"]))
+            fields = data["meta"].item()
+            fields.update({"__pos": pos[0]})
+            meta.update({ecg: fields})
+        return list_of_arrs, list_of_annotations, meta
 
     @action
     def dump(self, dst, fmt="npz"):
         """
-        Dumps each ecg in its own file with filename identical to the index
+        Save each ecg in its own file named as '<index>.<fmt>'
         """
         if fmt == "npz":
-            for ecg in self.index:
+            for ecg in self.indices:
+                signal, ann, meta = self[ecg]
+                del meta["__pos"]
                 np.savez(os.path.join(dst, ecg + "." + fmt),
-                         self[ecg][0][:, :self[ecg][2]["init_length"]],
-                         self[ecg][1],
-                         self[ecg][2])
+                         signal=signal,
+                         annotation=ann, meta=meta)
         else:
             raise NotImplementedError("The format is not supported yet")
 
     def __getitem__(self, index):
-        """
-        Indexation of ecgs by []
-        """
-        if isinstance(index, int):
-            if (np.min(list(self._ecg_index_number.values())) <= index
-                    <= np.max(list(self._ecg_index_number.values()))):
-                return (self._data[index, :, :],
-                        self._annotation if self._annotation.empty else
-                        self._annotation.loc[self._annotation.ecg ==
-                                             self._ecg_index_number[index], :],
-                        self._meta[index])
-            else:
-                raise IndexError("Index of ecg in the batch is out of range")
+        if index in self.indices:
+            pos = self._meta[index]['__pos']
+            return (self._data[pos],
+                    self._annotation if self._annotation.empty else self._annotation.loc[pos],
+                    self._meta[index])
         else:
-            if index in self.index:
-                return (self._data[self._ecg_index_number[index], :, :],
-                        self._annotation if self._annotation.empty else
-                        self._annotation.loc[self._annotation.ecg == index, :],
-                        self._meta[self._ecg_index_number[index]])
-            else:
-                raise IndexError(
-                    "There is no ecg with this index in the batch")
-
-    @property
-    def ecg_names_paths(self):
-        """
-        Return list of tuples containing ecg name and its data directory
-        """
-        return list(self._ecg_index_path.items())
-
-    @property
-    def ecg_indices(self):
-        """
-        Return ordered list of ecg names.
-        """
-        return list(self._ecg_index_number.keys())
-
-    @property
-    def ecg_paths(self):
-        """
-        Return ordered list of ecg data directories.
-        """
-        return list(self._ecg_index_path.values())
-
-    @property
-    def ecg_names_indexes(self):
-        """
-        Return list of tuples containing ecg index
-            and its number in batch.
-        """
-        return list(self._ecg_index_number.items())
+            raise IndexError("There is no such index in the batch", index)
