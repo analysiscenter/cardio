4a6bccfaa306415a513778f004a87644e34d5647
Alexandr Kuvaev <a.kuvaev@analysiscenter.ru>
2017-04-20 10:55:51 +0000

    Minor bugfixes and pylint config changes

--- models/autoencoder/autoencoder_factory.py
+++ models/autoencoder/autoencoder_factory.py
@@ -1,23 +1,27 @@
 from .keras_autoencoder import KerasAutoencoder, KerasVariationalAutoencoder
 
 
-class AutoencoderFactory:
+class AutoencoderFactory:  # pylint: disable=too-few-public-methods
+    backend_dict = None
+
     def __new__(cls, *args, backend="keras", **kwargs):
         if not isinstance(backend, str):
             raise TypeError("backend name must be a string")
+        if cls.backend_dict is None:
+            raise ValueError("backend dictionary must be defined")
         backend_cls = cls.backend_dict.get(backend)
         if backend_cls is None:
             raise KeyError("{} backend is not supported yet".format(backend))
         return backend_cls(*args, **kwargs)
 
 
-class Autoencoder(AutoencoderFactory):
+class Autoencoder(AutoencoderFactory):  # pylint: disable=too-few-public-methods
     backend_dict = {
         "keras": KerasAutoencoder,
     }
 
 
-class VariationalAutoencoder(AutoencoderFactory):
+class VariationalAutoencoder(AutoencoderFactory):  # pylint: disable=too-few-public-methods
     backend_dict = {
         "keras": KerasVariationalAutoencoder,
     }
--- models/autoencoder/keras_autoencoder.py
+++ models/autoencoder/keras_autoencoder.py
@@ -18,6 +18,33 @@ class KerasBaseAutoencoder(BaseAutoencoder):
     def __getattr__(self, name):
         return getattr(self.autoencoder, name)
 
+    @staticmethod
+    def _build_encoder(encoded, dims, activation):
+        for dim in dims:
+            encoded = Dense(dim, activation=activation)(encoded)
+        return encoded
+
+    @staticmethod
+    def _build_decoder(decoded, decoder, dims, hidden_activation, output_activation):
+        activations = [hidden_activation] * (len(dims) - 1) + [output_activation]
+        for dim, activation in zip(dims, activations):
+            layer = Dense(dim, activation=activation)
+            decoded = layer(decoded)
+            decoder = layer(decoder)
+        return decoded, decoder
+
+    @staticmethod
+    def _flatten(tensor):
+        if len(tensor.shape) >= 2:
+            return Flatten()(tensor)
+        return tensor
+
+    @staticmethod
+    def _reshape(tensor, shape):
+        if len(shape) >= 2:
+            return Reshape(shape)(tensor)
+        return tensor
+
     def fit(self, x, *args, **kwargs):
         if "validation_data" in kwargs:
             kwargs["validation_data"] = (kwargs["validation_data"], kwargs["validation_data"])
@@ -35,28 +62,24 @@ class KerasBaseAutoencoder(BaseAutoencoder):
 
 class KerasAutoencoder(KerasBaseAutoencoder):
     def __init__(self, input_shape, dims, hidden_activation, output_activation):
-        ndim = len(input_shape)
+        super().__init__()
 
+        # Encoder part
         encoder_input = Input(shape=input_shape)
-        if ndim >= 2:
-            encoded = Flatten()(encoder_input)
-        else:
-            encoder_input
-        for dim in dims:
-            encoded = Dense(dim, activation=hidden_activation)(encoded)
+        encoded = self._flatten(encoder_input)
+        encoded = self._build_encoder(encoded, dims, hidden_activation)
 
+        # Decoder part
         decoder_input = Input(shape=(dims[-1],))
         decoder = decoder_input
         decoded = encoded
+
         decoder_dims = dims[-2::-1] + [np.prod(input_shape)]
-        decoder_activations = [hidden_activation] * (len(decoder_dims) - 1) + [output_activation]
-        for dim, activation in zip(decoder_dims, decoder_activations):
-            layer = Dense(dim, activation=activation)
-            decoded = layer(decoded)
-            decoder = layer(decoder)
-        if ndim >= 2:
-            decoded = Reshape(input_shape)(decoded)
-            decoder = Reshape(input_shape)(decoder)
+        decoded, decoder = self._build_decoder(decoded, decoder, decoder_dims,
+                                               hidden_activation, output_activation)
+
+        decoded = self._reshape(decoded, input_shape)
+        decoder = self._reshape(decoder, input_shape)
 
         self.autoencoder = Model(input=encoder_input, output=decoded)
         self.encoder = Model(input=encoder_input, output=encoded)
@@ -64,37 +87,35 @@ class KerasAutoencoder(KerasBaseAutoencoder):
 
 
 class KerasVariationalAutoencoder(KerasBaseAutoencoder):
-    def __init__(self, input_shape, dims, hidden_activation):
-        ndim = len(input_shape)
+    def __init__(self, input_shape, dims, activation):
+        super().__init__()
+
+        # Encoder part
         encoder_input = Input(shape=input_shape)
         batch_size = tf.shape(encoder_input)[0]
-        if ndim >= 2:
-            encoded = Flatten()(encoder_input)
-        else:
-            encoded = encoder_input
-        for dim in dims:
-            encoded = Dense(dim, activation=hidden_activation)(encoded)
+        encoded = self._flatten(encoder_input)
+
+        encoded = self._build_encoder(encoded, dims, activation)
         z_mean = Dense(dims[-1])(encoded)
         z_log_std = Dense(dims[-1])(encoded)
 
-        def sample(args):
-            z_mean, z_log_std = args
-            z = K.random_normal(shape=(batch_size, dims[-1]), mean=0, std=1)
-            return z_mean + z * K.exp(z_log_std)
-        z = Lambda(sample)([z_mean, z_log_std])
+        # Sampling part
+        def sample_normal(args):
+            mean, log_std = args
+            return K.random_normal(shape=(batch_size, dims[-1]), mean=mean, std=K.exp(log_std))
+        z = Lambda(sample_normal)([z_mean, z_log_std])
 
+        # Decoder part
         decoder_input = Input(shape=(dims[-1],))
         decoder = decoder_input
         decoded = z
+
         decoder_dims = dims[-2::-1] + [np.prod(input_shape)]
-        decoder_activations = [hidden_activation] * (len(decoder_dims) - 1) + ["sigmoid"]
-        for dim, activation in zip(decoder_dims, decoder_activations):
-            layer = Dense(dim, activation=activation)
-            decoded = layer(decoded)
-            decoder = layer(decoder)
-        if ndim >= 2:
-            decoded = Reshape(input_shape)(decoded)
-            decoder = Reshape(input_shape)(decoder)
+        decoded, decoder = self._build_decoder(decoded, decoder, decoder_dims,
+                                               activation, "sigmoid")
+
+        decoded = self._reshape(decoded, input_shape)
+        decoder = self._reshape(decoder, input_shape)
 
         def loss(true, pred):
             reconstruction_loss = np.prod(input_shape) * metrics.binary_crossentropy(true, pred)
