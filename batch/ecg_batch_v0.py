d18c586311abc1164fea1a7200c4665c46662711
Egor Illarionov <e.illarionov@analysiscenter.ru>
2017-06-15 17:29:55 +0300

    loss and metrics evaluation

--- batch/ecg_batch_v0.py
+++ batch/ecg_batch_v0.py
@@ -35,60 +35,65 @@ from keras.callbacks import LearningRateScheduler
 sys.path.append('..')
 
 
-def Inception2D(x, base_dim, nb_filters, size_1, size_2, 
+def Inception2D(x, base_dim, nb_filters, size_1, size_2,
                 activation='linear', padding='same'):
     '''
     Inception block for 2D spectrogram.
     '''
-    conv_1 = Convolution2D(base_dim, (1, 1), 
+    conv_1 = Convolution2D(base_dim, (1, 1),
                            activation=activation, padding=padding)(x)
-    
-    conv_2 = Convolution2D(base_dim, (1, 1), 
+
+    conv_2 = Convolution2D(base_dim, (1, 1),
                            activation=activation, padding=padding)(x)
-    conv_2a = Convolution2D(nb_filters, (size_1, size_1), 
+    conv_2a = Convolution2D(nb_filters, (size_1, size_1),
                             activation=activation, padding=padding)(conv_2)
-    
-    conv_3 = Convolution2D(base_dim, (1, 1), 
+
+    conv_3 = Convolution2D(base_dim, (1, 1),
                            activation=activation, padding='same')(x)
-    conv_3a = Convolution2D(nb_filters, (size_2, size_2), 
+    conv_3a = Convolution2D(nb_filters, (size_2, size_2),
                             activation=activation, padding=padding)(conv_3)
-    
-    pool = MaxPooling2D(strides = (1, 1), padding=padding)(x)
-    conv_4 = Convolution2D(nb_filters, (1, 1), 
+
+    pool = MaxPooling2D(strides=(1, 1), padding=padding)(x)
+    conv_4 = Convolution2D(nb_filters, (1, 1),
                            activation=activation, padding=padding)(pool)
-    
-    concat = concatenate([conv_1, conv_2a, conv_3a, conv_4], axis=-1)    
+
+    concat = concatenate([conv_1, conv_2a, conv_3a, conv_4], axis=-1)
     return concat
+
 
 def fft(x):
     '''
-    tf fft 
+    tf fft
     '''
     import tensorflow as tf
     z = tf.map_fn(tf.transpose, tf.cast(x, dtype=tf.complex64))
     z2 = tf.cast(tf.abs(tf.fft(z)), dtype=tf.float32)
     return tf.map_fn(tf.transpose, z2)
 
+
 def rfft(x):
     '''
-    tf fft 
+    tf fft
     '''
     res = fft(x)
     half = int(res.get_shape().as_list()[1] / 2)
     return res[:, :half, :]
 
+
 def crop(x, a, b):
     '''
-    Crop 
+    Crop
     '''
     return x[:, a: b, :]
+
 
 def get_ecg(i, fields):
     data, annot, meta = fields
     pos = meta[i]['__pos']
     return (data[pos],
-           {k: v[pos] for k, v in annot.items()},
-           meta[i])
+            {k: v[pos] for k, v in annot.items()},
+            meta[i])
+
 
 def back_to_annot(arr, annot):
     res = []
@@ -96,34 +101,50 @@ def back_to_annot(arr, annot):
         res.append(annot[x == 1][0])
     return np.array(res)
 
+
 def validate(pred, testY, unq_classes):
-    labels = np.zeros(pred.shape, dtype = int)
+    labels = np.zeros(pred.shape, dtype=int)
     for i in range(len(labels)):
         labels[i, np.argmax(pred[i])] = 1
-        
+
     y_pred = back_to_annot(labels, unq_classes)
     if testY.ndim > 1:
         y_true = back_to_annot(testY, unq_classes)
     else:
         y_true = testY
-    
+
     print(classification_report(y_true, y_pred))
     print("f1_score", f1_score(y_true, y_pred, average='macro'))
+
+
+def f1_metric(pred, testY, unq_classes):
+    labels = np.zeros(pred.shape, dtype=int)
+    for i in range(len(labels)):
+        labels[i, np.argmax(pred[i])] = 1
+
+    y_pred = back_to_annot(labels, unq_classes)
+    if testY.ndim > 1:
+        y_true = back_to_annot(testY, unq_classes)
+    else:
+        y_true = testY
+
+    return f1_score(y_true, y_pred, average='macro')
+
 
 def resample_signal(signal, annot, meta, index, new_fs):
     """
     Resample signal to new_fs
     """
-    #signal, annot, meta = get_ecg(i, fields)
     fs = meta['fs']
     new_len = int(new_fs * len(signal[0]) / fs)
     signal = resample_poly(signal, new_len, len(signal[0]), axis=1)
     meta['fs'] = new_fs
     return [signal, annot, {index: meta}]
 
+
 def segment_signal(signal, annot, meta, index, length, step, pad):
     """
-    Segment signal 
+    Segment signal
     """
     diag = meta['diag']
     start = 0
@@ -131,18 +152,18 @@ def segment_signal(signal, annot, meta, index, length, step, pad):
     if(len(signal[0]) < length):
         if pad:
             pad_len = length - len(signal[0])
-            segments.append(np.lib.pad(signal, ((pad_len, 0), (0, 0)), 
+            segments.append(np.lib.pad(signal, ((pad_len, 0), (0, 0)),
                                        'constant', constant_values=(0, 0)))
             print(segments)
             return [np.array(segments), meta['diag'], index]
         else:
-            raise ValueError('Signal is shorter than segment length: %i < %i' 
-                             % (len(signal[0]), length))                           
+            raise ValueError('Signal is shorter than segment length: %i < %i'
+                             % (len(signal[0]), length))
     while(start + length <= len(signal[0])):
         segments.append(signal[:, start: start + length])
         start += step
     return [np.array(segments), meta['diag'], index]
-        
+
 
 class EcgBatch(Batch):
     """
@@ -157,38 +178,38 @@ class EcgBatch(Batch):
     def _signal(self):
         """ Signal """
         return self._data[0] if self._data is not None else None
-    
+
     @_signal.setter
     def _signal(self, value):
         """ Set labels """
-        data= list(self._data)
+        data = list(self._data)
         data[0] = value
         self._data = data
-    
+
     @property
     def _annotation(self):
         """ Annotation """
         return self._data[1] if self._data is not None else None
-    
+
     @_annotation.setter
     def _annotation(self, value):
         """ Set labels """
-        data= list(self._data)
+        data = list(self._data)
         data[1] = value
         self._data = data
-    
+
     @property
     def _meta(self):
         """ Meta """
         return self._data[2] if self._data is not None else None
-    
+
     @_meta.setter
     def _meta(self, value):
         """ Set labels """
-        data= list(self._data)
+        data = list(self._data)
         data[2] = value
         self._data = data
-    
+
     @staticmethod
     def create_annotation_df(data=None):
         """ Create a pandas dataframe with ECG annotations """
@@ -196,7 +217,6 @@ class EcgBatch(Batch):
 
     @action
     def load(self, src=None, fmt="wfdb"):
-        #print('Load', self.indices)
         """
         Loads data from different sources
         src is not used yet, so files locations are defined by the index
@@ -310,7 +330,7 @@ class EcgBatch(Batch):
                     self._meta[index])
         else:
             raise IndexError("There is no such index in the batch", index)
-    
+
     def update(self, data=None, annot=None, meta=None):
         """
         Update content of ecg_batch
@@ -325,7 +345,6 @@ class EcgBatch(Batch):
 
     def init_parallel(self, *args, **kwargs):
         return [list(self[i]) + [i] for i in self.indices]
-
 
     def post_parallel(self, all_results, *args, **kwargs):
         if any([isinstance(res, Exception) for res in all_results]):
@@ -349,31 +368,31 @@ class EcgBatch(Batch):
                                 annot=annot,
                                 meta=meta)
 
-    
     def post_parallel_segment(self, all_results, *args, **kwargs):
         if any([isinstance(res, Exception) for res in all_results]):
             print([res for res in all_results if isinstance(res, Exception)])
             return self
-        
+
         list_of_arrs = [x[0] for x in all_results]
         list_of_lens = [len(x[0]) for x in all_results]
-        
+
         list_of_labels = [x[1] for x in all_results]
-        
+
         list_of_origs = [x[2] for x in all_results]
-        
-        ind = DatasetIndex(index = np.arange(sum(list_of_lens), dtype=int))
-        
+
+        ind = DatasetIndex(index=np.arange(sum(list_of_lens), dtype=int))
+
         out_batch = EcgBatch(ind)
         data = np.concatenate(list_of_arrs)
         labels = np.repeat(list_of_labels, list_of_lens)
         origins = np.repeat(list_of_origs, list_of_lens)
-        meta = {i: {'__pos': i, 'diag': labels[i], 'origin': origins[i]} for i in range(len(labels))}
-        
+        meta = {i: {'__pos': i, 'diag': labels[i], 'origin': origins[i]}
+                for i in range(len(labels))}
+
         return out_batch.update(data=data,
                                 annot={},
                                 meta=meta)
-    
+
     @action
     @inbatch_parallel(init="init_parallel", post="post_parallel", target='mpc')
     def resample(self, new_fs):
@@ -383,10 +402,11 @@ class EcgBatch(Batch):
         return resample_signal
 
     @action
-    @inbatch_parallel(init="init_parallel", post="post_parallel_segment", target='mpc')
+    @inbatch_parallel(init="init_parallel", post="post_parallel_segment",
+                      target='mpc')
     def segment(self, length, step, pad):
         """
-        Segment all signals 
+        Segment all signals
         """
         return segment_signal
 
@@ -404,15 +424,8 @@ class EcgBatch(Batch):
 
     @model()
     def fft_inception():
-        def step_decay(epoch):
-            drop_t, lr = [[0, 50, 100], [0.01, 0.001, 0.0001]]
-            for i in range(len(drop_t)):
-                if drop_t[i] >= epoch:
-                    break
-            return lr[i]
-            
         input = Input((3000, 1))
-        
+
         conv_1 = Convolution1D(4, 4, activation='relu')(input)
         mp_1 = MaxPooling1D()(conv_1)
 
@@ -424,7 +437,8 @@ class EcgBatch(Batch):
 
         fft_1 = Lambda(rfft)(conv_4)
         shape_fft = fft_1.get_shape().as_list()
-        crop_1 = Lambda(crop, arguments={'a': 0, 'b': int(shape_fft[1] / 3)})(fft_1)
+        crop_1 = Lambda(crop,
+                        arguments={'a': 0, 'b': int(shape_fft[1] / 3)})(fft_1)
 
         shape_1d = crop_1.get_shape().as_list()[1:]
         shape_1d.append(1)
@@ -442,82 +456,102 @@ class EcgBatch(Batch):
 
         fc_1 = Dense(8, kernel_initializer='uniform', activation='relu')(pool)
         drop = Dropout(0.2)(fc_1)
-        
-        fc_2 = Dense(2, kernel_initializer='uniform', activation='softmax')(drop)
-        
+
+        fc_2 = Dense(2, kernel_initializer='uniform',
+                     activation='softmax')(drop)
+
         opt = Adam()
         model = Model(inputs=input, outputs=fc_2)
-        model.compile(optimizer=opt, loss="categorical_crossentropy")  
-        
-        lrate = LearningRateScheduler(step_decay)
-        hist = [] 
+        model.compile(optimizer=opt, loss="categorical_crossentropy")
+
+        hist = {'train_loss': [], 'val_loss': [], 'val_metric': []}
         diag_code = {'A': 'A', 'N': 'nonA', 'O': 'nonA'}
-        
-        return model, lrate, hist, diag_code
-    
+
+        lr_schedule = [[0, 50, 100], [0.01, 0.001, 0.0001]]
+
+        return model, hist, diag_code, lr_schedule
+
     @action()
     def get_labels(self, encode=None):
         dsY = []
         for ind in self.indices:
-            diag = self._meta[ind]['diag'] 
+            diag = self._meta[ind]['diag']
             if encode is None:
                 dsY.extend([diag])
             else:
                 dsY.extend([encode[diag]])
-        
+
         if encode is None:
             pos_labels = []
         else:
             pos_labels = list(np.unique(list(encode.values())))
-        dsY =  pos_labels + dsY   
+        dsY = pos_labels + dsY
         unq_classes, num_labels = np.unique(dsY, return_inverse=True)
         catY = np_utils.to_categorical(num_labels)[len(pos_labels):]
         return catY, unq_classes
 
-
     @action(model='fft_inception', singleton=True)
     def train_fft_inception(self, model_comp, nb_epoch, batch_size):
-        model, lrate, hist, code = model_comp
+        model, hist, code, lr_s = model_comp
         trainX = self._signal.reshape((-1, 3000, 1))
         trainY, enq_classes = self.get_labels(encode=code)
-        hist.append(model.fit(trainX, trainY,
-                              epochs=nb_epoch, batch_size=batch_size,
-                              callbacks=[lrate]))
+        epoch_num = len(hist['train_loss'])
+        if epoch_num in lr_s[0]:
+            new_lr = lr_s[1][lr_s[0].index(epoch_num)]
+            opt = Adam(lr=new_lr)
+            model.compile(optimizer=opt, loss="categorical_crossentropy")
+
+        res = model.fit(trainX, trainY,
+                        epochs=nb_epoch,
+                        batch_size=batch_size)
+        hist['train_loss'].append(res.history["loss"][0])
         return self
 
     @action(model='fft_inception')
     def print_summary(self, model_comp):
         print(model_comp[0].summary())
         return self
-    
+
     @action(model='fft_inception')
     def calc_loss(self, model_comp):
-        model, lrate, hist, code = model_comp
+        model, hist, code, _ = model_comp
         testX = self._signal.reshape((-1, 3000, 1))
         testY, unq_classes = self.get_labels(encode=code)
         pred = model.predict(testX)
-        print('loss:', log_loss(testY, pred))
+        hist['val_loss'].append(log_loss(testY, pred))
+        hist['val_metric'].append(f1_metric(pred, testY, unq_classes))
         #print(model.evaluate(testX, testY, batch_size=batch_size))
         return self
-    
+
+    @action(model='fft_inception')
+    def train_report(self, model_comp):
+        model, hist, code, _ = model_comp
+        if len(hist['train_loss']) == 0:
+            print('Train history is empty')
+        else:
+            print('Epoch', len(hist['train_loss']))
+            print('train_loss: %3.2f   val_loss: %3.2f   val_metric: %3.2f'
+                  % (hist['train_loss'][-1], hist['val_loss'][-1],
+                     hist['val_metric'][-1]))
+        return self
+
     @action(model='fft_inception')
     def print_accuracy(self, model_comp):
-        model, lrate, hist, code = model_comp
+        model, hist, code, _ = model_comp
         testX = self._signal.reshape((-1, 3000, 1))
         testY, unq_classes = self.get_labels(encode=code)
         pred = model.predict(testX)
         validate(pred, testY, unq_classes)
         return self
-    
+
     @action(model='fft_inception')
     def show_loss(self, model_comp):
-        model, lrate, hist, code = model_comp
+        model, hist, _, _ = model_comp
         fig = plt.figure()
-        plt.plot(hist[0].history["loss"], "r", label="train loss")
-        #plt.plot(hist[0].history["val_loss"], "b", label="validation loss")
+        plt.plot(hist["train_loss"], "r", label="train loss")
+        plt.plot(hist["val_loss"], "b", label="validation loss")
         plt.legend()
-        plt.title ("Model loss")
+        plt.title("Model loss")
         plt.ylabel("Loss")
         plt.xlabel("Epoch")
         plt.show()
-    
