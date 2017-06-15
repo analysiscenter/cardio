3c313af257d1796559b4cee60b54ea9c1c6faaa7
Dmitry Podvyaznikov <d.podvyaznikov@analysiscenter.ru>
2017-06-15 12:19:26 +0300

    Updated dataset submodule, changed code structure

--- batch/ecg_batch.py
+++ batch/ecg_batch.py
@@ -1,7 +1,12 @@
 """ contain Batch class for processing ECGs """
+import os
+import sys
+import numpy as np
+import pandas as pd
 import wfdb
 sys.path.append('..')
 from dataset import Batch, action, inbatch_parallel, any_action_failed
+
 
 class EcgBatch(Batch):
     """
@@ -14,16 +19,61 @@ class EcgBatch(Batch):
         self._init_data()
         self.history = []
 
-
     def _init_data(self, data=None, annotation=None, meta=None):
         self._data = data
-        self._annotation = self.create_annotation_df() if annotation is None else annotation
+        self._annotation = self.create_annotation_df(
+        ) if annotation is None else annotation
         self._meta = dict() if meta is None else meta
 
     @staticmethod
     def create_annotation_df(data=None):
         """ Create a pandas dataframe with ECG annotations """
         return pd.DataFrame(data=data, columns=["ecg", "index", "value"])
+
+    def loader_init(self, *args, **kwargs):  #pylint: disable=unused-argument
+        """
+        Init method for parallelism in loading
+        """
+
+        init_indices = [[(init_val[0], init_val[1])]
+                        for init_val in np.ndenumerate(self.indices)]
+        return init_indices
+
+    def loader_post(self, list_of_results, *args, **kwargs):  #pylint: disable=unused-argument
+        """
+        Post method for parallelism in loading
+        """
+        if any_action_failed(list_of_results):
+            raise ValueError("Failed while parallelizing: ",
+                             self.get_errors(list_of_results))
+
+        array_of_results = np.array(list_of_results)
+        data = array_of_results[:, 1]
+        annot = pd.concat(array_of_results[:, 2])
+        meta = dict(zip(array_of_results[:, 0], array_of_results[:, 3]))
+
+        self._init_data(data, annot, meta)
+
+        return self
+
+    def default_post(self, list_of_results, *args, **kwargs):  #pylint: disable=unused-argument
+        """
+        Default post for parallelism: collect results, make a numpy array
+        and change self._data attribute to it.
+        """
+        # Check if all elements of the resulting list are numpy arrays.
+        # If not - throw an exception, otherwise rewrite self._data attribute.
+
+        if any_action_failed(list_of_results):
+            raise ValueError("Could not assemble the batch: ",
+                             self.get_errors(list_of_results))
+
+        if all(isinstance(x, np.ndarray) for x in list_of_results):
+            self._data = np.array(list_of_results)
+        else:
+            raise ValueError(
+                "List of results contains non-numpy.ndarray elements.")
+        return self
 
     @action
     def load(self, src=None, fmt="wfdb"):
@@ -32,23 +82,23 @@ class EcgBatch(Batch):
         src is not used yet, so files locations are defined by the index
         """
         if fmt == "wfdb":
-            self._load_wfdb(src=src) # pylint: disable=no-value-for-parameter
+            self._load_wfdb(src=src)  # pylint: disable=no-value-for-parameter
         elif fmt == "npz":
-            self._load_npz(src=src) # pylint: disable=no-value-for-parameter
+            self._load_npz(src=src)  # pylint: disable=no-value-for-parameter
         else:
             raise TypeError("Incorrect type of source")
 
         # add info in self.history
         info = dict()
         info['method'] = 'load'
-        info['params'] = {"src":src, "fmt":fmt}
+        info['params'] = {"src": src, "fmt": fmt}
         self.history.append(info)
 
         return self
 
     @action
-    @inbatch_parallel(init='loader_init', post='loader_post', target='async')
-    async def _load_wfdb(self, ix, src=None, *args, **kwargs):
+    @inbatch_parallel(init='loader_init', post='loader_post', target='threads')
+    def _load_wfdb(self, index, src=None):
         ecg = index[1]
         pos = index[0]
         if src:
@@ -63,14 +113,14 @@ class EcgBatch(Batch):
             annot = wfdb.rdann(path, "atr")
         except FileNotFoundError:
             annot = self.create_annotation_df()  # pylint: disable=redefined-variable-type
-        
+
         fields.update({"__pos": pos[0]})
 
         return (ecg, signal, annot, fields)
 
     @action
     @inbatch_parallel(init='loader_init', post='loader_post', target='threads')
-    def _load_npz(self, ix, src=None, *args, **kwargs):
+    def _load_npz(self, index, src=None):
         ecg = index[1]
         pos = index[0]
 
@@ -84,32 +134,8 @@ class EcgBatch(Batch):
         annot = self.create_annotation_df(data["annotation"])
         fields = data["meta"].item()
         fields.update({"__pos": pos[0]})
-        
+
         return (ecg, signal, annot, fields)
-
-    def loader_init(self, *args, **kwargs): #pylint: disable=unused-argument
-        """
-        Init method for parallelism in loading
-        """
-        
-        init_indices = [[(init_val[0], init_val[1])] for init_val in np.ndenumerate(self.indices)]
-        return init_indices
-
-    def loader_post(self, list_of_results, *args, **kwargs): #pylint: disable=unused-argument
-        """
-        Post method for parallelism in loading
-        """
-        if any_action_failed(list_of_results):
-            raise ValueError("Failed while parallelizing: ", self.get_errors(list_of_results))
-
-        array_of_results = np.array(list_of_results)
-        data = array_of_results[:, 1]
-        annot = pd.concat(array_of_results[:, 2])
-        meta = dict(zip(array_of_results[:, 0],array_of_results[:, 3]))
-
-        self._init_data(data, annot, meta)
-
-        return self
 
     @action
     def dump(self, dst, fmt="npz"):
@@ -117,7 +143,7 @@ class EcgBatch(Batch):
         Save each ecg in its own file named as '<index>.<fmt>'
         """
         if fmt == "npz":
-            self._dump_npz(dst=dst) # pylint: disable=no-value-for-parameter
+            self._dump_npz(dst=dst)  # pylint: disable=no-value-for-parameter
         else:
             raise NotImplementedError("The format is not supported yet")
 
@@ -128,10 +154,11 @@ class EcgBatch(Batch):
     def _dump_npz(self, index, dst):
         signal, ann, meta = self[index]
         del meta["__pos"]
-        np.savez(os.path.join(dst, index + ".npz"),
-                 signal=signal,
-                 annotation=ann,
-                 meta=meta)
+        np.savez(
+            os.path.join(dst, index + ".npz"),
+            signal=signal,
+            annotation=ann,
+            meta=meta)
 
     def __getitem__(self, index):
         if index in self.indices:
@@ -144,22 +171,6 @@ class EcgBatch(Batch):
         else:
             raise IndexError("There is no such index in the batch", index)
 
-    def default_post(self, list_of_results, *args, **kwargs):  #pylint: disable=unused-argument
-        """
-        Default post for parallelism: collect results, make a numpy array
-        and change self._data attribute to it.
-        """
-        # Check if all elements of the resulting list are numpy arrays.
-        # If not - throw an exception, otherwise rewrite self._data attribute.
-        if any_action_failed(list_of_results):
-            raise ValueError("Could not assemble the batch: ", self.get_errors(list_of_results))
-
-        if all(isinstance(x, np.ndarray) for x in list_of_results):
-            self._data = np.array(list_of_results)
-        else:
-            raise ValueError("List of results contains non-numpy.ndarray elements.")
-        return self
-
     @action
     @inbatch_parallel(init='indices', post='default_post', target='threads')
     def generate_subseqs(self, index, length, step):
@@ -170,12 +181,5 @@ class EcgBatch(Batch):
         """
         sig = self[index][0]
         n_splits = np.int((sig.shape[1] - length) / step) + 1
-        splits = np.array([
-            np.array(sig[:, i * step:(i * step + length)]) for i in range(n_splits)
-        ])
+        splits = np.array([np.array(sig[:, i * step:(i * step + length)]) for i in range(n_splits)])
         return splits
-
-import numpy as np
-import pandas as pd
-import sys
-import os
