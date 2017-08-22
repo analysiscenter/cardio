861694640c8dd9e6f1d7b064d9e9bf0f7f8b7574
Dmitry Podvyaznikov <d.podvyaznikov@analysiscenter.ru>
2017-08-22 19:07:29 +0300

    Fixing structure

--- ecg/dataset/__init__.py
+++
@@ -1,2 +0,0 @@
-""" Dataset """
-from .dataset import * # pylint: disable=wildcard-import
--- ecg/dataset/dataset/image/__init__.py
+++
@@ -1,2 +0,0 @@
-""" Contains image types and constants """
-from .batch_image import ImagesBatch, ImagesPILBatch, CROP_00, CROP_CENTER
