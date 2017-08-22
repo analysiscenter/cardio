c15b03076ba000fd1d72cda75dc09f711fb07e65
Alexandr Kuvaev <a.kuvaev@analysiscenter.ru>
2017-08-22 18:11:54 +0300

    Updated metrics

--- ecg/models/metrics.py
+++ ecg/models/metrics.py
@@ -2,46 +2,46 @@ import numpy as np
 import sklearn
 
 
-def get_class_prob(predictions_dict, metric_name="Metric"):
-    class_prob = predictions_dict.get("class_prob")
-    if class_prob is None:
-        raise ValueError("{} is only applicable to classification tasks".format(metric_name))
-    return class_prob
+def get_class_prob(predictions_dict):
+    true_dict = predictions_dict.get("target_true")
+    pred_dict = predictions_dict.get("target_pred")
+    if true_dict is None or pred_dict is None:
+        raise ValueError("Each element of predictions list must be a dict with target_true and target_pred keys")
+    return true_dict, pred_dict
 
 
-def get_labels(predictions_list, metric_name="Metric"):
-    labels_list = []
+def get_labels(predictions_list):
+    true_labels = []
+    pred_labels = []
     for predictions_dict in predictions_list:
-        class_prob = get_class_prob(predictions_dict, metric_name)
-        labels_list.append(max(class_prob, key=class_prob.get))
-    return np.array(labels_list)
+        true_dict, pred_dict = get_class_prob(predictions_dict)
+        true_labels.append(max(true_dict, key=true_dict.get))
+        pred_labels.append(max(pred_dict, key=pred_dict.get))
+    return np.array(true_labels), np.array(pred_labels)
 
 
 def get_probs(predictions_list, metric_name="Metric"):
-    probs_list = []
+    true_probs = []
+    pred_probs = []
     for predictions_dict in predictions_list:
-        class_prob = get_class_prob(predictions_dict, metric_name)
-        probs_list.append([class_prob[key] for key in sorted(class_prob.keys())])
-    return np.array(probs_list)
+        true_dict, pred_dict = get_class_prob(predictions_dict)
+        true_probs.append([true_dict[key] for key in sorted(true_dict.keys())])
+        pred_probs.append([pred_dict[key] for key in sorted(pred_dict.keys())])
+    return np.array(true_probs), np.array(pred_probs)
 
 
-def f1_score(y_true, y_pred, average="macro", **kwargs):
-    y_true = get_labels(y_true, metric_name="F1 score")
-    y_pred = get_labels(y_pred, metric_name="F1 score")
-    labels = sorted(set(y_true) | set(y_pred))
-    return sklearn.metrics.f1_score(y_true, y_pred, labels=labels, average=average, **kwargs)
+def f1_score(predictions_list, average="macro", **kwargs):
+    true_labels, pred_labels = get_labels(predictions_list)
+    unique_labels = sorted(set(true_labels) | set(pred_labels))
+    return sklearn.metrics.f1_score(true_labels, pred_labels, labels=unique_labels, average=average, **kwargs)
 
 
-def auc(y_true, y_pred, average="macro", **kwargs):
-    y_true = get_probs(y_true, metric_name="AUC")
-    y_pred = get_probs(y_pred, metric_name="AUC")
-    return sklearn.metrics.roc_auc_score(y_true, y_pred, average=average, **kwargs)
+def auc(predictions_list, average="macro", **kwargs):
+    return sklearn.metrics.roc_auc_score(*get_probs(predictions_list), average=average, **kwargs)
 
 
-def classification_report(y_true, y_pred, **kwargs):
-    y_true = get_labels(y_true, metric_name="Classification report")
-    y_pred = get_labels(y_pred, metric_name="Classification report")
-    return sklearn.metrics.classification_report(y_true, y_pred, **kwargs)
+def classification_report(predictions_list, **kwargs):
+    return sklearn.metrics.classification_report(*get_labels(predictions_list), **kwargs)
 
 
 METRICS_DICT = {
@@ -51,7 +51,7 @@ METRICS_DICT = {
 }
 
 
-def calculate_metrics(metrics_list, y_true, y_pred):
+def calculate_metrics(metrics_list, predictions_list):
     metrics_res = []
     for metric in metrics_list:
         if isinstance(metric, str):
@@ -62,5 +62,5 @@ def calculate_metrics(metrics_list, y_true, y_pred):
             metric_fn = metric
         else:
             raise ValueError("Unknown metric type")
-        metrics_res.append(metric_fn(y_true, y_pred))
+        metrics_res.append(metric_fn(predictions_list))
     return metrics_res
