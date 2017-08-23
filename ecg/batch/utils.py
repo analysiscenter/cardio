c59b94cc1f04501917e2dfd74003f14ea4295766
Alexandr Kuvaev <a.kuvaev@analysiscenter.ru>
2017-08-23 11:18:57 +0300

    Added utils docstrings

--- ecg/batch/utils.py
+++ ecg/batch/utils.py
@@ -1,10 +1,26 @@
+"""Miscellaneous ecg batch utils."""
+
 import numpy as np
 import sklearn
 
 
 class LabelBinarizer(sklearn.preprocessing.LabelBinarizer):
+    """Encode categorical features using a one-hot scheme."""
     # pylint: disable=invalid-name
+
     def transform(self, y):
+        """Transform y using one-hot encoding.
+
+        Parameters
+        ----------
+        y : 1-D ndarray of shape [n_samples,]
+            Class labels.
+
+        Returns
+        -------
+        Y : 2-D array of shape [n_samples, n_classes]
+            One-hot encoded labels.
+        """
         Y = super().transform(y)
         if len(self.classes_) == 1:
             Y = 1 - Y  # pylint: disable=redefined-variable-type
@@ -13,6 +29,20 @@ class LabelBinarizer(sklearn.preprocessing.LabelBinarizer):
         return Y
 
     def inverse_transform(self, Y, threshold=None):
+        """Transform one-hot encoded labels back to class labels.
+
+        Parameters
+        ----------
+        Y : 2-D array of shape [n_samples, n_classes]
+            One-hot encoded labels.
+        threshold : float or None
+            Threshold used in the binary and multi-label cases.
+
+        Returns
+        -------
+        y : 1-D ndarray of shape [n_samples,]
+            Class labels.
+        """
         if len(self.classes_) == 1:
             y = super().inverse_transform(1 - Y, threshold)
         elif len(self.classes_) == 2:
