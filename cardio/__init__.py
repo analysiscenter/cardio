854d65884b81c4962260213af7adee0ca1f33f1a
Dmitry Podvyaznikov <d.podvyaznikov@analysiscenter.ru>
2017-11-09 12:57:35 +0300

    Updated shippabe; added version of the CardIO

--- cardio/__init__.py
+++ cardio/__init__.py
@@ -2,3 +2,10 @@
 from .batch import * # pylint: disable=wildcard-import
 from . import dataset as ds
 from .models import * # pylint: disable=wildcard-import
+
+
+__version__ = '0.1.0'
+
+
+if sys.version_info < (3, 5):
+    raise ImportError("Dataset module requires Python 3.5 or higher")
