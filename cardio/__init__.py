005cbeb9a7c8917a0373acfc1ac9d8888bb23b5b
Alexandr Kuvaev <a.kuvaev@analysiscenter.ru>
2017-11-09 15:54:29 +0300

    Changed import error text

--- cardio/__init__.py
+++ cardio/__init__.py
@@ -1,13 +1,13 @@
 """ ECG package """
 import sys
 
-from .batch import * # pylint: disable=wildcard-import
+from .batch import *  # pylint: disable=wildcard-import
 from . import dataset as ds
-from .models import * # pylint: disable=wildcard-import
+from .models import *  # pylint: disable=wildcard-import
 
 
 __version__ = '0.1.0'
 
 
 if sys.version_info < (3, 5):
-    raise ImportError("Dataset module requires Python 3.5 or higher")
+    raise ImportError("CardIO package requires Python 3.5 or higher")
