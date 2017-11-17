df19b2dc03df4b69b689d23a6d8bbbbff75fd124
illarionovEA <e.illarionov@analysiscenter.ru>
2017-11-17 12:05:48 +0000

    fix setup

--- setup.py
+++ setup.py
@@ -4,19 +4,14 @@ Documentation - https://analysiscenter.github.io/cardio/
 """
 
 from setuptools import setup, find_packages
-from os import path
 import re
-#from pypandoc import convert_file
 
-
-here = path.abspath(path.dirname(__file__))
-
-with open('dataset/__init__.py', 'r') as f:
+with open('cardio/__init__.py', 'r') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 
-#long_description = convert_file(path.join(here, 'README.md'), 'rst')
-
+with open('docs/index.rst', 'r') as f:
+    long_description = f.read()
 
 
 setup(
@@ -25,18 +20,19 @@ setup(
     version=version,
     url='https://github.com/analysiscenter/cardio',
     license='Apache License 2.0',
-    author='Data Analysis Center',
+    author='Roman Kh at al',
     author_email='rhudor@gmail.com',
     description='A framework for deep research of electrocardiograms',
-    #long_description=long_description,
+    long_description=long_description,
     zip_safe=False,
     platforms='any',
     install_requires=[
         'numpy>=1.10',
-        'wfdb>=1.2.2.'
+        'wfdb>=1.2.2.',
+        'hmmlearn>=0.2.0'
     ],
     extras_require={
-        'tensorflow': ['tensorflow>=1.4'],
+        'tensorflow': ['tensorflow>=1.13'],
         'tensorflow-gpu': ['tensorflow-gpu>=1.13'],
         'keras': ['keras>=2.0.0']
     },
