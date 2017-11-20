6e29ca25422bbb0b8ffeacc5062be5421adcdede
Alexandr Kuvaev <a.kuvaev@analysiscenter.ru>
2017-11-20 17:51:31 +0300

    Added EcgDataset docstring

--- cardio/batch/ecg_dataset.py
+++ cardio/batch/ecg_dataset.py
@@ -5,6 +5,30 @@ from .ecg_batch import EcgBatch
 
 
 class EcgDataset(ds.Dataset):
+    """ECG Dataset class.
+
+    Contains indices of ECGs to perform actions from specified batch class on.
+
+    Parameters
+    ----------
+    index : DatasetIndex or None, optional
+        Unique identifiers of ECGs in a dataset. If ``index`` is not given, it
+        is constructed by instantiating ``index_class`` with ``args`` and
+        ``kwargs``.
+    batch_class : type, optional
+        Class of batches, constructed by a dataset. Must be inherited from
+        ``Batch``.
+    preloaded : tuple, optional
+        Data to put in created batches. Defaults to ``None``.
+    index_class : type, optional
+        Class of built index if ``index`` is not given. Must be inherited from
+        ``DatasetIndex``.
+    args : misc, optional
+        Additional positional argments to ``index_class.__init__``.
+    kwargs : misc, optional
+        Additional named argments to ``index_class.__init__``.
+    """
+
     def __init__(self, index=None, batch_class=EcgBatch, preloaded=None, index_class=ds.FilesIndex, *args, **kwargs):
         if index is None:
             index = index_class(*args, **kwargs)
