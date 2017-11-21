b2f1856f17b61bbcab7cedef90be242a4707e742
Dmitry Podvyaznikov <d.podvyaznikov@analysiscenter.ru>
2017-11-21 13:51:03 +0300

    Update cardio/init file

--- cardio/__init__.py
+++ cardio/__init__.py
@@ -6,7 +6,3 @@ from . import dataset  # pylint: disable=wildcard-import
 
 
 __version__ = '0.1.0'
-
-
-if sys.version_info < (3, 5):
-    raise ImportError("CardIO package requires Python 3.5 or higher")
