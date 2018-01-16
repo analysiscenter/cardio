1bdb266adc81fa4b9d58a8c09c8b93bca667038a
Dmitry Podvyaznikov <d.podvyaznikov@analysiscenter.ru>
2018-01-16 17:00:42 +0300

    Changed html theme

--- docs/conf.py
+++ docs/conf.py
@@ -79,13 +79,14 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
-#html_theme = 'nature'
-html_theme = 'bizstyle'
 
-#html_theme_options = {
+html_theme = 'nature'
+#html_theme = 'bizstyle'
+
+html_theme_options = {
 #    "rightsidebar": "false"
-#}
+    'sidebarwidth': 290
+}
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
